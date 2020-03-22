28c7ac77e4866f9378c81e194e1360d475f9d091
Angel Ulise Tabares Gonzalez <taho2509@gmail.com>
2020-03-22 20:49:50 -0300

    add about menu

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -17,6 +17,9 @@
               <li class="nav-item">
                 <a class="nav-link js-scroll-trigger" href="#recomendations">Recomendaciones</a>
               </li>
+              <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="#about">Acerca de</a>
+              </li>
             </ul>
           </div>
         </div>
