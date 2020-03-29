2a2014a167889d2db5dbfb1786c6fb7803566715
José Andrés Suárez Alberteris <jaalberteris@gmail.com>
2020-03-29 10:41:57 -0400

    Margen

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -114,7 +114,7 @@
             </div>
           </div>
         </div>
-        <div class="row">
+        <div class="row mt-2">
           <div class="col-12 col-md-6 col-lg-5 ml-lg-auto">
               <div class="card">
                 <div class="card-body"> 
@@ -130,7 +130,7 @@
             </div>
           </div>
         </div>
-        <div class="container">
+        <div class="container mt-2">
           <div class="row">
             <div class="col-lg-12 mx-auto">
               <div class="card">
